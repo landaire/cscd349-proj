60459b2b5e92a703fd05442a8fcfef7c7a624404
Lander Brandt <landerbrandt@gmail.com>
2015-03-17 10:32:20 -0700

    Fix step resetting

--- src/main/java/com/cscd/game/event/listener/PartyMoveListener.java
+++ src/main/java/com/cscd/game/event/listener/PartyMoveListener.java
@@ -13,9 +13,10 @@ import java.util.Random;
  * Created by Lander Brandt on 3/12/15.
  */
 public class PartyMoveListener implements Observer {
-    private static int steps = 100;
     private static int encounters = 0;
     private static Random rand = new Random(System.currentTimeMillis());
+    private static final int stepsForRandomEvent = 100;
+    private static int steps = stepsForRandomEvent;
 
     @Override
     public void update(Observable o, Object arg) {
@@ -26,7 +27,7 @@ public class PartyMoveListener implements Observer {
         // TODO: logic for limiting encounters?
 
         if (steps == 0) {
-            steps = 10;
+            steps = stepsForRandomEvent;
 
             // 25% chance of firing this event
             if (rand.nextInt(101) > 75) {
